9a0991e16b8b01e6a4af85d2a66dc59692041f15
KIMINJOONG <ijboym216@gmail.com>
2019-03-08 23:42:12 +0900

    Creating a Tab Navigation

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AppLoading, Font, Asset } from "expo";
 import { Ionicons } from "@expo/vector-icons";
-import { StyleSheet, Text, View } from 'react-native';
+import TabNavigation from "./navigation/TabNavigation";
 
 export default class App extends React.Component {
   state = {
@@ -26,9 +26,7 @@ export default class App extends React.Component {
     const {loaded} = this.state;
     if(loaded) {
       return (
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-        </View>
+        <TabNavigation />
       );
     } else {
       return (
@@ -37,12 +35,3 @@ export default class App extends React.Component {
     }
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
