90f852c442e2b129170e9fad6b00cf65d7dc9f2a
KIMINJOONG <ijboym216@gmail.com>
2019-03-09 20:33:36 +0900

    Making a Slider part One

--- screens/Movie/MoviesPresenter.js
+++ screens/Movie/MoviesPresenter.js
@@ -2,10 +2,25 @@ import React from "react";
 import { Text } from "react-native";
 import PropTypes from "prop-types";
 import Loader from "../../components/Loader";
+import styled from "styled-components";
+import MovieSlider from "../../components/MovieSlider";
+
+const Container = styled.ScrollView`
+    background-color: black;
+`;
 
 
 
-const MoviesPresenter = ({loading, upcoming, popular, nowPlaying})  => loading ? <Loader /> : <Text>Movies</Text>;
+const MoviesPresenter = ({loading, upcoming, popular, nowPlaying})  => 
+    loading ? (
+        <Loader />
+    ) : (
+        <Container>
+            <MovieSlider 
+                movies={nowPlaying}
+            />
+        </Container>
+    );
 
 MoviesPresenter.propTypes = {
     loading: PropTypes.bool.isRequired,
