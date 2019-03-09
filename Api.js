5457eda2bef53fd4aadf5a01435f022bed3097ec
KIMINJOONG <ijboym216@gmail.com>
2019-03-09 12:31:48 +0900

    API Verbs part One

--- Api.js
+++ Api.js
@@ -7,3 +7,16 @@ const api = axios.create({
         language: "ko-KR"
     }
 });
+
+export const moviesApi = {
+    nowPlaying: () => api.get("movie/now_playing"),
+    upcoming: () => api.get("movie/upcoming"),
+    popular: () => api.get("movie/popular")
+
+};
+
+export const tvApi = {
+    topRated: () => api.get("tv/top_rated"),
+    popular: () => api.get("tv/popular"),
+    airingToday: () => api.get("tv/airing_today")
+};
